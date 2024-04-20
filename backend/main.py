87f600910f0b0ea60c14e7d86d44342c86bb0d38
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-04-21 04:40:06 +0530

    added backend

---
+++ backend/main.py
@@ -0,0 +1,63 @@
+# uvicorn main:app
+# uvicorn main:app --reload
+
+# Main imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+
+# Custom function imports
+from functions.openai_requests import get_chat_response
+from functions.database import store_messages, reset_messages
+
+
+# Get Environment Vars
+openai.organization = config("OPEN_AI_ORG")
+openai.api_key = config("OPEN_AI_KEY")
+
+
+# Initiate App
+app = FastAPI()
+
+
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:3000",
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Check health
+@app.get("/health")
+async def check_health():
+    return {"response": "healthy"}
+
+
+# Reset Conversation
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()
+    return {"response": "conversation reset"}
+
+
+
+
+@app.post("/chat/")
+async def chat(input_text: str):
+    response = get_chat_response(input_text)
+    return {"response": response}
